0f71afab852dadd4253abdb10f1563c1716d5c50
miuka <d.lolimiuku@gmail.com>
2022-11-24 18:37:09 +0900

    better pandoc syntax

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -5,3 +5,15 @@ vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
 vim.wo.spell = true
 vim.bo.spelllang = 'en_us,cjk'
+
+-- pandoc sytanx setting
+vim.g['pandoc#syntax#conceal#blacklist'] = {
+    'atx',
+    'ellipses',
+    'emdashes',
+    'codeblock_start',
+    'codeblock_delim',
+}
+vim.g.tex_conceal='amgs' --disable equation conceal 
+vim.g['pandoc#syntax#codeblocks#embeds#langs'] =
+    { 'html', 'python', 'bash=sh', 'c', 'cpp', 'latex=tex', 'diff' }
