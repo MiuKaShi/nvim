49d5a87b5c62a026e20dfb519fe2a6db3ea7b967
miuka <d.lolimiuku@gmail.com>
2022-12-11 20:31:13 +0900

    fix cmp and treesitter problem

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -75,10 +75,7 @@ return require('packer').startup(function(use)
     use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
-    use {
-        'hrsh7th/cmp-cmdline',
-        commit = 'd2dfa338520c99c1f2dc6af9388de081a6e63296',
-    }
+    use 'hrsh7th/cmp-cmdline'
     use 'hrsh7th/cmp-path'
     use 'lukas-reineke/cmp-rg'
     use 'saadparwaiz1/cmp_luasnip'
--- lua/user/aerial.lua
+++ lua/user/aerial.lua
@@ -4,20 +4,12 @@ if not status_ok then
 end
 
 aerial.setup {
-    filter_kind = {
-        'Class',
-        'Constant',
-        'Constructor',
-        'Enum',
-        'EnumMember',
-        'Function',
-        'Interface',
-        'Method',
-        'Module',
-        'Object',
-        'Struct',
-        'Variable',
-    },
+    min_width = 30,
+    -- backends = {"lsp", "treesitter", "markdown"}
+    backends = { 'treesitter', 'markdown' },
+    filter_kind = false,
+    nerd_font = 'auto',
+    update_events = 'TextChanged,InsertLeave',
     on_attach = function(bufnr)
         vim.keymap.set('n', '<leader>o', function()
             require('aerial').toggle()
